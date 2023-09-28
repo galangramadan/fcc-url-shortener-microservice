a5178910e178360e91efcfba48d1517e2fbb7ebe
galangramadan <galangynr@gmail.com>
2023-09-28 23:06:01 +0700

    feat: hostname validation

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:shorturl", function (req, res) {});
+
+app.post("/api/shorturl", function (req, res) {
+  async function hostnameValidation(hostname) {
+    return new Promise((resolve) => {
+      dns.lookup(hostname, (err) => {
+        if (err) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+
+  try {
+    const url = new URL(req.body.url);
+    const hostname = url.hostname;
+
+    hostnameValidation(hostname)
+      .then((valid) => {
+        if (!valid) {
+          throw new Error("Invalid hostname");
+        }
+
+        const response = {
+          original_url: req.body.url,
+          short_url: 1,
+        };
+
+        res.send(response);
+      })
+      .catch((err) => {
+        res.send({ error: err.message });
+      });
+  } catch (err) {
+    res.send({
+      error: err.message,
+    });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
